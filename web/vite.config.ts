649eb02c021904b92dd34f53b4f6522c866192da
KONFeature <quentin@nivelais.com>
2025-11-12 19:00:10 +0100

    Github page deployment

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 
 const config = defineConfig({
+  base: '/',
   plugins: [
     devtools(),
-    TanStackRouterVite(),
+    tanstackRouter(),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
